554682dacc721bdb2877f0e8d549d7a5873e67f2
JiJogbf <alex.gas1995@ya.ru>
2021-01-07 19:02:29 +0300

    Implementing basement for test runner

--- src/cpp/test/test.cpp
+++ src/cpp/test/test.cpp
@@ -1,3 +1,11 @@
+#include <gas/test/Logger.hpp>
+#include <gas/test/FileLogger.hpp>
+#include <gas/test/Tester.hpp>
+
 int main(){
+    gas::test::Logger* logger = new gas::test::FileLogger("result.log");
+    gas::test::Tester* tester;
+    tester->run();
+    delete tester;
     return 0;
 }
